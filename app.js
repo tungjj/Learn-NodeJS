f6538f0f85a5732104cfbe7d395da902c771d408
tungjj <49489457+tungjj@users.noreply.github.com>
2021-06-16 18:08:46 +0700

    chatRealtime Initial

--- app.js
+++ app.js
@@ -4,14 +4,21 @@ var bodyParser = require('body-parser')
 require('./mongoose')
 const user_route = require('./routes/user')
 const port = process.env.PORT || 3000
-
+const socket = require('socket.io')
 
 app.use(express.json());
 app.use('/user', user_route)
 
-app.get('/', (req, res)=>{res.send('hello')})
+// use static
+app.use(express.static("public"));
 
-app.listen(port, () =>
+
+// socket
+const server = app.listen(port, () =>
 {
     console.log('Server is up on port'+port)
+})
+const io = socket(server)
+io.on('connection', (socket) => {
+    console.log("Made a connection")
 })
